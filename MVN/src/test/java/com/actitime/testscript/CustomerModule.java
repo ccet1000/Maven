fba8d0562946fd1cb62aa7fc20c225505cf0f624
QSP <QSP@DESKTOP-NFCI9UD.local>
2024-08-07 10:51:47 +0530

    Added a new test method

--- MVN/src/test/java/com/actitime/testscript/CustomerModule.java
+++ MVN/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -8,4 +8,8 @@ public class CustomerModule {
 public void createCustomer() {
 	Reporter.log("createCustomer",true);
 }
+	@Test
+public void deleteCustomer() {
+	Reporter.log("createCustomer",true);
+}
 }
